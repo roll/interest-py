d08e9a666a585c898269d0b5691e6a9a60503179
roll <roll@respect31.com>
2015-01-10 14:08:28 +0300

    added example

---
+++ demo/example.py
@@ -0,0 +1,27 @@
+import logging
+from interest import Service, Resource, Middleware, get
+
+
+class Auth(Middleware):
+
+    # Public
+
+    def process_request(self, request):
+        request.hello = True
+        return request
+
+
+class Comment(Resource):
+
+    # Public
+
+    @get('/{id}')
+    def read(self, request):
+        return {'hello': request.hello}
+
+
+service = Service(path='/api/v1')
+service.add_middleware(Auth)
+service.add_resource(Comment)
+service.logger.setLevel(logging.DEBUG)
+service.listen(hostname='127.0.0.1', port=9000)
