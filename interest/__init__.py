e8b9d913e1f7741da53eec0f7d61f1e984ad5b97
roll <roll@respect31.com>
2015-01-11 12:41:34 +0300

    added match to API

--- interest/__init__.py
+++ interest/__init__.py
@@ -1,6 +1,7 @@
 from .binding import Binding, get, post, put, delete, patch, head, options
 from .dispatcher import Dispatcher
 from .formatter import Formatter, JSONFormatter
+from .match import Match, ExistentMatch, NonExistentMatch
 from .middleware import Middleware
 from .processor import Processor
 from .protocol import Protocol
