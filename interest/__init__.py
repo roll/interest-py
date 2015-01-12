3b8d72423055c585643158653f67320c73dcf896
roll <roll@respect31.com>
2015-01-12 10:50:55 +0300

    added dispathcer package

--- interest/__init__.py
+++ interest/__init__.py
@@ -1,10 +1,9 @@
-from .binding import Binding, get, post, put, delete, patch, head, options
-from .dispatcher import Dispatcher
+from .dispatcher import (Binding, get, post, put, delete, patch, head, options,
+                         Match, ExistentMatch, NonExistentMatch,
+                         Dispatcher, Resource)
 from .formatter import Formatter, JSONFormatter
-from .match import Match, ExistentMatch, NonExistentMatch
 from .middleware import Middleware
 from .processor import Processor
 from .protocol import Protocol
-from .resource import Resource
 from .service import Service
 version = '0.0.0'  # REPLACE: version = '{{ version }}'
---
+++ interest/dispatcher/__init__.py
@@ -0,0 +1,4 @@
+from .binding import Binding, get, post, put, delete, patch, head, options
+from .dispatcher import Dispatcher
+from .match import Match, ExistentMatch, NonExistentMatch
+from .resource import Resource
