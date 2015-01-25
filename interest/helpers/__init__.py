d2b856470d0877062f3e4fd0028e4abc713d8753
roll <roll@respect31.com>
2015-01-25 19:09:04 +0300

    improved match

--- interest/helpers/__init__.py
+++ interest/helpers/__init__.py
@@ -1,5 +1,6 @@
 from .cache import cachedproperty
 from .chain import Chain
 from .http import http
+from .match import Match, ExistentMatch, NonExistentMatch
 from .order import OrderedMetaclass
 from .plugin import PluginImporter
--- interest/helpers/match.py
+++ interest/helpers/match.py
@@ -1,6 +1,26 @@
-class Match(dict):
+from abc import ABCMeta, abstractmethod
+
+
+class Match(dict, metaclass=ABCMeta):
+
+    # Public
+
+    @abstractmethod
+    def __bool__(self):
+        pass  # pragma: no cover
+
+
+class ExistentMatch(Match):
 
     # Public
 
     def __bool__(self):
         return True
+
+
+class NonExistentMatch(Match):
+
+    # Public
+
+    def __bool__(self):
+        return False
